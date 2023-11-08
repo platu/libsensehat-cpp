473e4da351f1e2478bda4951e22e6b8c3a208cea
Philippe Latu <philippe.latu@inetdoc.net>
2023-11-08 12:01:23 +0100

    Update lab01.cpp back to default

--- labTemplate/lab01.cpp
+++ labTemplate/lab01.cpp
@@ -20,46 +20,23 @@ using namespace std::chrono; // system_clock, seconds, milliseconds
 
 
 int main() {
-	// Declare your variables below !
-	const rgb_pixel_t R = { .color = {255, 0, 0} }; // Red
-	const rgb_pixel_t W = { .color = {255, 255, 255} }; // White
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Début variables
 
-	rgb_pixels_t question_mark_RedOnWhite = { .array = {
-		{ W, W, W, R, R, W, W, W },
-		{ W, W, R, W, W, R, W, W },
-		{ W, W, W, W, W, R, W, W },
-		{ W, W, W, W, R, W, W, W },
-		{ W, W, W, R, W, W, W, W },
-		{ W, W, W, R, W, W, W, W },
-		{ W, W, W, W, W, W, W, W },
-		{ W, W, W, R, W, W, W, W } }
-	};
-
-	int count;
-	// End of variables declarations
+	// Fin variables
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	if(senseInit()) {
 		cout << "Sense Hat initialization Ok." << endl;
 		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// Insert your code below !
+		// Début instructions
 
-		count = 0;
 
-		do {
-			senseSetRGBpixels(question_mark_RedOnWhite);
-
-			sleep_for(seconds(1));
-
-			question_mark_RedOnWhite = senseFlip_v(false);
-
-			count = count + 1;
-		} while (count < 5);
-
+		// Fin instructions
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		cout << "Press joystick button to quit." << endl;
 		senseWaitForJoystickEnter();
 
-		// Insert your code above !
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		senseShutdown();
 		cout << "Sense Hat shut down." << endl;
 	}
