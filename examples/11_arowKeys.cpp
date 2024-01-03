66225c6ffea2b95a8be237d75c308204df22e86b
platu <philippe.latu@inetdoc.net>
2024-01-03 18:29:58 +0100

    Add separate include file for console events

--- examples/11_arowKeys.cpp
+++ examples/11_arowKeys.cpp
@@ -11,96 +11,13 @@
 
 #include <iostream>
 #include <thread> // sleep_for, sleep_until
-#include <sys/ioctl.h>
-#include <termios.h>
 
 #include <sensehat.h>
+#include <console_io.h>
 
 using namespace std;
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // system_clock, milliseconds
-
-/// \brief Arrow key code list
-enum arrowKey {UP, DOWN, LEFT, RIGHT, OTHER};
-
-/// \brief Clear the console screen and place the cursor at the top left
-void clearConsole() {
-	cout << "\x1b[2J\x1b[0;0f" << flush;
-}
-
-/// \brief Clear the line from the cursor position to the end of line
-void clearEOL() {
-	cout << "\x1b[K" << flush;
-}
-
-/// \brief Set cursor position to (x,y) in the console
-/// \param x Horizontal position or column
-/// \param y Vertical position or row
-void gotoxy(int x, int y) {
-	cout << "\x1b[" << y << ';' << x << 'f' << flush;
-}
-
-/// \brief Non-blocking keyboard input detection
-/// \return Number of bytes waiting in the keyboard buffer
-int keypressed() {
-
-	static const int STDIN = 0;
-	static bool initialized = false;
-	termios term;
-	int bytesWaiting;
-
-	if (! initialized) {
-		// Deactivate buffered input
-		tcgetattr(STDIN, &term);
-		term.c_lflag &= (tcflag_t)~ICANON;
-		tcsetattr(STDIN, TCSANOW, &term);
-		setbuf(stdin, NULL);
-		// Synchronize with C I/O
-		cin.sync_with_stdio();
-
-		initialized = true;
-	}
-
-	ioctl(STDIN, FIONREAD, &bytesWaiting);
-	return bytesWaiting;
-}
-
-/// \brief Return the arrow key code from the keyboard buffer if any
-/// \return arrowKey code among UP, DOWN, LEFT, RIGHT, OTHER
-arrowKey getArrowKey() {
-
-	char c;
-	arrowKey code;
-
-	// Read first character
-	c = getchar();
-	// Test for escape sequence
-	if (c == '\x1b') {
-		// Read second character '\\'
-		c = getchar();
-		// Read third character '['
-		c = getchar();
-		// Read fourth character with arrow key code
-		switch(c) {
-			case 'A':
-				code = UP;
-				break;
-			case 'B':
-				code = DOWN;
-				break;
-			case 'C':
-				code = RIGHT;
-				break;
-			case 'D':
-				code = LEFT;
-				break;
-			default:
-				code = OTHER;
-		}
-	}
-	fflush(stdin);
-	return code;
-}
 
 // Main program
 int main() {
