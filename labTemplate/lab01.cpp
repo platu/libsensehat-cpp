daabb144883b52b609e186438bd0c4c2a0be4611
Philippe Latu <philippe.latu@inetdoc.net>
2025-11-05 10:21:12 +0100

    Refactor lab01.cpp with French comments and structure
    Updated the lab01.cpp template with comments in French and organized the structure for variable declarations and subprograms.

--- labTemplate/lab01.cpp
+++ labTemplate/lab01.cpp
@@ -2,9 +2,7 @@
  * Author: Philippe Latu
  * Source: https://github.com/platu/libsensehat-cpp
  *
- * Student lab template source file
- *
- * This program waits for the joystick button to be pressed
+ * Patron du code source des séances de travaux pratiques
  */
 
 #include <iostream>
@@ -18,46 +16,35 @@ using namespace std;
 using namespace std::this_thread;  // sleep_for, sleep_until
 using namespace std::chrono;       // system_clock, seconds, milliseconds
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Début constantes
+
+// Fin constantes
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Début sous-programmes
+
+// Fin sous-programmes
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 int main() {
-    // Declare your variables below !
-    const rgb_pixel_t R = {.color = {255, 0, 0}};      // Red
-    const rgb_pixel_t W = {.color = {255, 255, 255}};  // White
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Début variables
 
-    rgb_pixels_t question_mark_RedOnWhite = {
-        .array = {{W, W, W, R, R, W, W, W},
-                  {W, W, R, W, W, R, W, W},
-                  {W, W, W, W, W, R, W, W},
-                  {W, W, W, W, R, W, W, W},
-                  {W, W, W, R, W, W, W, W},
-                  {W, W, W, R, W, W, W, W},
-                  {W, W, W, W, W, W, W, W},
-                  {W, W, W, R, W, W, W, W}}};
-
-    int count;
-    // End of variables declarations
+    // Fin variables
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     if (senseInit()) {
         cout << "Sense Hat initialization Ok." << endl;
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // Insert your code below !
+        // Début instructions
 
-        count = 0;
-
-        do {
-            senseSetRGBpixels(question_mark_RedOnWhite);
-
-            sleep_for(seconds(1));
-
-            question_mark_RedOnWhite = senseFlip_v(false);
-
-            count = count + 1;
-        } while (count < 5);
-
+        // Fin instructions
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         cout << "Press joystick button to quit." << endl;
         senseWaitForJoystickEnter();
 
-        // Insert your code above !
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         senseShutdown();
         cout << "Sense Hat shut down." << endl;
     }
